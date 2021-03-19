35250fefae264cd9c07f7bd3516f0e34292f6156
HieuSoi <dinhhoanghieu321@gmail.com>
2021-03-19 15:46:55 +0700

    tho

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,33 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Trong nhân thế chỉ đôi lần ta khóc
+Khi chào đời khó nhọc mẹ khai sinh
+Lúc song thân tạ thế khuất ánh nhìn
+Rồi sẽ sống vì mình mà nở nụ
+
+Cười chua chát dù tim nhàu héo rũ
+Cho vui lòng bạn hữu với anh em
+Cười mỉm chi trước kỳ thị pha dèm
+Ngẩng cao đầu mặc ai xem chướng mắt
+
+Cười khoan dung với những người không thật
+Sống hai lòng ngoài mặt giả nghĩa nhân
+Cười thứ tha cho những kẻ bất phân
+Trộn gia vị mấy lần thêm mắm muối
+
+Cười cay đắng cho chợ đời nữa buổi
+Nhiều sắc màu đá cuội lẫn vàng thau
+Cười hả hê cho cuộc sống lao đao
+Đem thật giả nặn nhàu sai đen trắng
+
+Cười chê chán lòng hờn ghen oán hận
+Trước nhu mì sau vạch sẵn mưu mô
+Giấu giận hờn sau làn nước mắt khô
+Ta vẫn cứ trầm trồ cười nghiêng ngã.
+        }
     }
 }
